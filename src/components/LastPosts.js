d31256689d1a7eff907fed2479f6b8a1ff8f0799
German Mora <germanmora@Germans-Air.home>
2021-08-18 13:28:11 +0200

    arreglos de css

--- src/components/LastPosts.js
+++ src/components/LastPosts.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 const LastPosts = props => (
   <>
-    <h2 className='text-center subHeader'>ÚLTIMOS POSTS</h2>
+    <h2 className='text-center subHeader mb-2'>ÚLTIMOS POSTS</h2>
     {props.posts.length > 0 && (
       <div>
         <div className='pb-6'>
           <div>
             {props.posts.map(({ node }) => (
-              <div key={node.id} className='mb-2'>
+              <div key={node.id} className='mb-3'>
                 <div className='posts align-items-start'>
                   {node.frontmatter.featuredImage && (
                     <div className='mb-1'>
@@ -22,7 +22,12 @@ const LastPosts = props => (
                     </Link>
                   </h2>
                   <p>{node.frontmatter.date}</p>
-                  <div className='post-content'>{node.excerpt}</div>
+                  <div className='post-content'>
+                    {node.excerpt}{' '}
+                    <Link to={node.fields.slug} className='post-title'>
+                      Leer más
+                    </Link>
+                  </div>
                 </div>
               </div>
             ))}
