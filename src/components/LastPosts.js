36cbec52e1a8694854b5f66d0fbcbc912e9e9042
German Mora <germanmora@Germans-Air.home>
2021-08-18 13:00:57 +0200

    cambios de estilo

--- src/components/LastPosts.js
+++ src/components/LastPosts.js
@@ -1,48 +1,36 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-const LastPosts = (props) => {
-    return (
-        <>
-            <h2>ÚLTIMOS POSTS</h2>
-            {props.posts.length > 0 && (
-                <div>
-                    <div className='pb-6'>
-                        <div>
-                            {props.posts.map(({ node }) => (
-                                <div key={node.id} className='mb-2'>
-                                    <div className='posts align-items-start'>
-                                        {node.frontmatter.featuredImage && (
-                                            <div className='mb-1'>
-                                                <img
-                                                    src={
-                                                        node.frontmatter
-                                                            .featuredImage
-                                                    }
-                                                />
-                                            </div>
-                                        )}
-                                        <h2 className='text-dark'>
-                                            <Link
-                                                to={node.fields.slug}
-                                                className='post-title'
-                                            >
-                                                {node.frontmatter.title}
-                                            </Link>
-                                        </h2>
-                                        <p>{node.frontmatter.date}</p>
-                                        <div className='post-content'>
-                                            {node.excerpt}
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
+const LastPosts = props => (
+  <>
+    <h2 className='text-center subHeader'>ÚLTIMOS POSTS</h2>
+    {props.posts.length > 0 && (
+      <div>
+        <div className='pb-6'>
+          <div>
+            {props.posts.map(({ node }) => (
+              <div key={node.id} className='mb-2'>
+                <div className='posts align-items-start'>
+                  {node.frontmatter.featuredImage && (
+                    <div className='mb-1'>
+                      <img src={node.frontmatter.featuredImage} />
                     </div>
+                  )}
+                  <h2 className='text-dark'>
+                    <Link to={node.fields.slug} className='post-title'>
+                      {node.frontmatter.title}
+                    </Link>
+                  </h2>
+                  <p>{node.frontmatter.date}</p>
+                  <div className='post-content'>{node.excerpt}</div>
                 </div>
-            )}
-        </>
-    );
-};
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )}
+  </>
+);
 
 export default LastPosts;
