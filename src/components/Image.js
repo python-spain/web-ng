507565329d9cb90ea59686be80758a808f5adb38
unknown <stefanobelfiore@yahoo.com>
2021-09-17 15:06:58 +0200

    acabado imagen y hazte socio

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,16 +1,11 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 import '../scss/style.scss';
 
 const Image = () => {
 
-    return <StaticImage
-            src="../images/python.jpg"
-            alt="Python"
-            placeholder="blurred"
-            layout="fixed"
-            style={{ width: '100%', height: '100%',  zoom:'80%', paddingTop:'50rem' }}
-            imgStyle={{ objectFit: 'cover' }} />
+    return <div className="div-background-image">
+        <div className="background-image"></div>
+    </div>
 
 }
 export default Image
