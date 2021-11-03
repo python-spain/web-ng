d89f3f817f49d32e18c5c6d1a198e555cf4ce5fa
carme <carmecodina.p@gmail.com>
2021-11-03 16:06:16 +0100

    Revert "renderizado condicional en el componente FeaturedEvents"
    This reverts commit 9ad40fafa1fc9794c06d84a2291e2f04c10cfaf9.

--- src/components/FeaturedEvents.js
+++ src/components/FeaturedEvents.js
@@ -6,27 +6,20 @@ import EventPost from './EventPost';
 const FeaturedEvents = ({ eventos }) => {
     return (
         <div className='featured-events'>
-            {window.innerWidth > 991 ? (
-                <Carousel className='featured-events-slider'>
-                    {eventos.map((evento) => (
-                        <Carousel.Item>
-                            <EventSlide evento={evento} key={evento.id} />
-                        </Carousel.Item>
+            <Carousel className='featured-events-slider'>            
+                {eventos.map(evento => (
+                <Carousel.Item>  
+                    <EventSlide evento={evento} key={evento.id}/>
+                </Carousel.Item>
+                ))}
+            </Carousel>
+            <div className='featured-events-mobile'>
+                <div className="row cols-1 row-cols-sm-2 g-3">
+                    {eventos.map(evento => (
+                        <EventPost evento={evento} key={evento.id} featured={true}/>
                     ))}
-                </Carousel>
-            ) : (
-                <div className='featured-events-mobile'>
-                    <div className='row cols-1 row-cols-sm-2 g-3'>
-                        {eventos.map((evento) => (
-                            <EventPost
-                                evento={evento}
-                                key={evento.id}
-                                featured={true}
-                            />
-                        ))}
-                    </div>
-                </div>
-            )}
+                </div>                
+            </div>
         </div>
     );
 };
