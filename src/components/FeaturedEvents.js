e12a6464491377b0d50e74a6a16a9ed8f3667907
carme <carmecodina.p@gmail.com>
2021-11-03 16:08:30 +0100

    formateo prettier

--- src/components/FeaturedEvents.js
+++ src/components/FeaturedEvents.js
@@ -6,19 +6,23 @@ import EventPost from './EventPost';
 const FeaturedEvents = ({ eventos }) => {
     return (
         <div className='featured-events'>
-            <Carousel className='featured-events-slider'>            
-                {eventos.map(evento => (
-                <Carousel.Item>  
-                    <EventSlide evento={evento} key={evento.id}/>
-                </Carousel.Item>
+            <Carousel className='featured-events-slider'>
+                {eventos.map((evento) => (
+                    <Carousel.Item>
+                        <EventSlide evento={evento} key={evento.id} />
+                    </Carousel.Item>
                 ))}
             </Carousel>
             <div className='featured-events-mobile'>
-                <div className="row cols-1 row-cols-sm-2 g-3">
-                    {eventos.map(evento => (
-                        <EventPost evento={evento} key={evento.id} featured={true}/>
+                <div className='row cols-1 row-cols-sm-2 g-3'>
+                    {eventos.map((evento) => (
+                        <EventPost
+                            evento={evento}
+                            key={evento.id}
+                            featured={true}
+                        />
                     ))}
-                </div>                
+                </div>
             </div>
         </div>
     );
