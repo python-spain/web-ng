bee2c3887a837547e9a786300accc2c5808bd763
Israel Saeta Pérez <dukebody@gmail.com>
2021-11-10 09:06:39 +0100

    Correción id widget newsletter.

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -14,21 +14,21 @@ const Newsletter = ({ children }) => {
         <>
             <data
                 id='mj-w-res-data'
-                data-token='2722fe3c3b864c03ac395a1f56b195f9'
+                data-token='1ed34321b65ae94833b87a960f78136c'
                 className='mj-w-data'
                 data-apikey='5JG8'
-                data-w-id='IT4'
+                data-w-id='K5l'
                 data-lang='es_ES'
                 data-base='https://app.mailjet.com'
                 data-width='640'
-                data-height='437'
+                data-height='433'
                 data-statics='statics'
                 style={{ position: 'absolute', top: 0 }}
             ></data>
 
             <div
                 className='mj-w-button mj-w-btn'
-                data-token='2722fe3c3b864c03ac395a1f56b195f9'
+                data-token='1ed34321b65ae94833b87a960f78136c'
             >
                 <div>
                     <div>{children}</div>
