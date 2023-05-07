d5b00ab877c1bd335fe9a94f26f9f4f4c1488480
Israel Saeta Pérez <dukebody@gmail.com>
2023-05-07 15:33:17 +0200

    Abrir subscripción a newsletter en pestaña nueva.
    En la versión de Gatsby instalada el nodo "Link" ignora el atributo "target". Usamos un simple "a" en estos casos para que siempre abramos el enlace en pestaña nueva en estos casos.

--- src/components/FeaturesHome.js
+++ src/components/FeaturesHome.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+const MyLink = (props) => {
+    // en la versión instalada de Gatsby, Link ignora el atributo "target", por
+    // lo que usamos un simple <a> en su lugar
+    const feature = props.feature;
+    if (feature.target) {
+        return (
+            <a
+                href={feature.url}
+                className='content'
+                target={feature.target ?? '_self'}
+            >
+                {props.children}
+            </a>
+        );
+    } else {
+        return (
+            <Link
+                to={feature.url}
+                className='content'
+                target={feature.target ?? '_self'}
+            >
+                {props.children}
+            </Link>
+        );
+    }
+};
+
 const FeaturesHome = ({ features }) => (
     <div className='features-home'>
         <div className='row row-cols-1 row-cols-sm-2 row-cols-lg-3 gy-2 justify-content-center'>
             {features.map((feature, i) => (
-                <Link
-                    to={feature.url}
-                    className='content'
-                    key={i}
-                    target={feature.target ?? '_self'}
-                >
+                <MyLink feature={feature} key={i}>
                     <img src={feature.image} />
                     <h3>{feature.title}</h3>
-                </Link>
+                </MyLink>
             ))}
         </div>
     </div>
