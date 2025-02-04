0402f2174c3b6b588cf5273d9c5f8c31ef862896
EllaQuimica <EllaQuimica@users.noreply.github.com>
2025-02-04 19:32:02 +0000

    deployed via Gatsby Publish Action 🎩 for be94d75ede538ba298e879f3fec2b7e658741de1

---
+++ component---src-pages-404-js-e84564d692aea93158ea.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_serif_theme=self.webpackChunkgatsby_serif_theme||[]).push([[883],{9616:function(e,t,n){n.r(t);var s=n(7294),a=n(3145);class l extends s.Component{render(){return s.createElement(a.Z,null,s.createElement("h1",null,"Not Found"),s.createElement("p",null,"You just hit a page that doesn't exist..."))}}t.default=l}}]);
+//# sourceMappingURL=component---src-pages-404-js-e84564d692aea93158ea.js.map
